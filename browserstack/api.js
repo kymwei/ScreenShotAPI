b3dca9e8532c689adc782f8f341acb5a7f66b671
kymwei <kymwei1002@gmail.com>
2017-04-21 01:38:04 -0700

    screen shot interval works~

--- browserstack/api.js
+++ browserstack/api.js
@@ -3,11 +3,11 @@ var Credentials = require("./credentials.js");
 
 var screenshotClient = BrowserStack.createScreenshotClient(Credentials.browserStackCredentials());
 var restclient = BrowserStack.createClient(Credentials.browserStackCredentials());
-var websiteUrl = "https://test01.dev.kbb.com";
+var websiteUrl = "http://www.chefmoomoo.com/";
 
 var WebClient = require('@slack/client').WebClient;
 
-var token = process.env.SLACK_API_TOKEN || 'xoxp-173151743030-172346769218-172422642532-454b7a0b1fcd4fa270a55681746ac10e'; //see section above on sensitive data
+var token = process.env.SLACK_API_TOKEN || 'xoxp-173151743030-172346769218-172561175796-d868febf3ccf01eaa281e7895364fb46'; //see section above on sensitive data
 
 var web = new WebClient(token);
 
@@ -23,14 +23,13 @@ screenshotClient.getBrowsers(function(error, browsers) {
 
 	// use screenshot api
 
-	//screenshotClient.generateScreenshots(options, generateScreenshotsCallback);
+	screenshotClient.generateScreenshots(options, generateScreenshotsCallback);
 
 
 	// use rest api workers
 	//restclient.createWorker(latestChromeBrowser, createWorkerCallback);
 
-	//get job back 3ab6173996ed7bdb3c9ae4af882ae0ff38506a7c
-    screenshotClient.getJob("3ab6173996ed7bdb3c9ae4af882ae0ff38506a7c", screenShotJobCallcallback)
+
 });
 
 
@@ -39,13 +38,20 @@ function screenShotJobCallcallback(error, job) {
     if(error) {
         console.log(error.stack);
     }else{
-        web.chat.postMessage('#general', job.screenshots[0].image_url, function(err, res) {
-            if (err) {
-                console.log('Error:', err);
-            } else {
-                console.log('Message sent: ', res);
-            }
-        });
+    	debugger
+		if(job.screenshots[0].image_url) {
+            web.chat.postMessage('#general', job.screenshots[0].image_url, function (err, res) {
+                if (err) {
+                    console.log('Error:', err);
+                } else {
+                    console.log(count);
+                    console.log('Message sent: ', res);
+
+                }
+            });
+            clearInterval(getScreenShot);
+
+        }
     	console.log(job);
 	}
 }
@@ -55,13 +61,19 @@ function createWorkerCallback(error, worker) {
 		console.log(error.stack);
 	}
 }
-
+var getScreenShot;
+var count = 0;
 function generateScreenshotsCallback(error, job) {
 	debugger
 	if(error) {
 		console.log(error.stack);
 	}else{
 
+        getScreenShot = setInterval(function() {
+                screenshotClient.getJob(job.job_id, screenShotJobCallcallback)
+                count ++;
+            }
+            , 1000);
 	}
 }
 
