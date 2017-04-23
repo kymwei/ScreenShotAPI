e4b72fcddb2f1d2207c232b7a07549ef4621aada
kymwei <kymwei1002@gmail.com>
2017-04-23 02:28:27 -0700

    more

--- browserstack/controllers/browserstackController.js
+++ browserstack/controllers/browserstackController.js
@@ -3,6 +3,10 @@
 var BrowserStack = require("browserstack");
 var Credentials = require("../credentials.js");
 var screenshotClient = BrowserStack.createScreenshotClient(Credentials.browserStackCredentials());
+var slackToken = require("../slack.js");
+var token = process.env.SLACK_API_TOKEN || slackToken.slack.token; //see section above on sensitive data
+var WebClient = require('@slack/client').WebClient;
+var web = new WebClient(token);
 
 exports.list_all_browsers = function(req, res){
     var data = {'res':'hi cutie pie'};
@@ -13,3 +17,91 @@ exports.list_all_browsers = function(req, res){
     });
 
 };
+
+exports.getScreenShot = function(req, res){
+    var user_id = req.body.username;
+    var url = req.body.url;
+
+    screenshotClient.getBrowsers(function(error, browsers) {
+        var latestChromeBrowser = GetWindows7LatestChromeBrowser(browsers);
+        var browsersToScreenshot = [];
+        browsersToScreenshot.push(latestChromeBrowser);
+
+        var options = {};
+        options.browsers = browsersToScreenshot;
+        options.url = url;
+        options.local = true;
+
+        // use screenshot api
+        console.log(options);
+        screenshotClient.generateScreenshots(options, generateScreenshotsCallback);
+        res.json(user_id + 'screen for ' + url +' is processing');
+
+    });
+
+};
+
+
+function screenShotJobCallcallback(error, job) {
+
+    if(error) {
+        console.log(error.stack);
+    }else{
+        debugger
+        if(job.screenshots[0].image_url) {
+            web.chat.postMessage('#general', job.screenshots[0].image_url, function (err, res) {
+                if (err) {
+                    console.log('Error:', err);
+                } else {
+                    console.log(count);
+                    console.log('Message sent: ', res);
+
+                }
+            });
+            clearInterval(getScreenShot);
+
+        }
+        console.log(job);
+    }
+}
+
+var getScreenShot;
+var count = 0;
+function generateScreenshotsCallback(error, job) {
+    debugger
+    if(error) {
+        console.log(error.stack);
+    }else{
+
+        getScreenShot = setInterval(function() {
+                screenshotClient.getJob(job.job_id, screenShotJobCallcallback)
+                count ++;
+            }
+            , 3000);
+    }
+}
+
+function GetWindows7LatestChromeBrowser(browsers){
+    // filter chrome browsers
+
+    var chromeBrowsers = browsers.filter(function (el) {
+        return el.browser == 'chrome',
+        el.os_version == '7'}
+    );
+
+    var highestVersion = 0.0;
+    for(var i = 0; i < chromeBrowsers.length - 1; i++) {
+        var version = chromeBrowsers[i].browser_version;
+        if(!isNaN(version)) {
+            if(parseFloat(version) > highestVersion) {
+                highestVersion = parseFloat(version);
+            }
+        }
+    }
+
+    var latestBrowser = chromeBrowsers.filter(function (el) {
+        return el.browser_version == highestVersion}
+    )[0];
+
+    return(latestBrowser);
+}
--- browserstack/routes/browserstackRountes.js
+++ browserstack/routes/browserstackRountes.js
@@ -3,7 +3,23 @@
 module.exports = function(app){
     var browserstack = require('../controllers/browserstackController');
 
+    app.get('/', function (req, res) {
+        res.send('hi cutie pie')
+    })
+
     //browserstack api Rounts
     app.route('/browsers')
         .get(browserstack.list_all_browsers);
+
+    app.route('/generateScreenShot')
+        .get(function (req, res) {
+            console.log(req);
+            res.send('Get a random book')
+        })
+        .post(browserstack.getScreenShot)
+        .put(function (req, res) {
+            res.send('Update the book')
+        })
 };
+
+
