8ca118edc026c0053ee05bd7ed58d6d5352fbe41
raymond.tam <raymond.tam@kbb.com>
2017-04-24 18:28:33 -0700

    call browserstack end point when i get the slack command

--- slackapp/server.js
+++ slackapp/server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser')
-var http = require('http');
+var request = require('request');
 var app = express();
 var PORT = 1234
 
-var browserStackGenerateScreenShotEndPoint = 'http://chefmoomoo.com:1002/generateScreenShot'
+var browserStackGenerateScreenShotEndPoint = 'http://10.228.150.7:300/generatescreenshot'
+
+app.route('/BrowserstackScreenshotComplete')
+    .get(function (req, res) {
+        res.sendStatus(200)
+    })
+    .post(bodyParser.urlencoded({ extended: true }), function (req, res) {
+
+    })
+
+
 app.route('/SlackEndPoint')
     .get(function (req, res) {
         res.sendStatus(200)
@@ -28,42 +38,18 @@ app.route('/SlackEndPoint')
         // post to browserStackGenerateScreenShotEndPoint
         // url=www.yahoo.com
         // username=raytam
-
-        var requestData = {
-            "url": url,
-            "username": user
-        }
-
-        // QPX REST API URL (I censored my api key)
-        url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=myApiKey"
-
-        // fire request
-        request({
-            url: browserStackGenerateScreenShotEndPoint,
-            json: true,
-            multipart: {
-                chunked: false,
-                data: [
-                    {
-                        'content-type': 'application/json',
-                        body: requestData
-                    }
-                ]
+        request.post(
+            browserStackGenerateScreenShotEndPoint,
+            { json: { url: url, username: user } },
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body)
+                }
             }
-        }, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                console.log(body)
-            }
-            else {
-
-                console.log("error: " + error)
-                console.log("response.statusCode: " + response.statusCode)
-                console.log("response.statusText: " + response.statusText)
-            }
-        })
+        );
     })
 
-app.listen(PORT, function (er) {
+app.listen(PORT, function (err) {
     if (err) {
         return console.error('Error starting server: ', err)
     }
