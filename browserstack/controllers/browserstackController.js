54f04e112f93d5d13c8cb6585c353750daa2003d
kymwei <kymwei1002@gmail.com>
2017-04-25 04:02:11 -0700

    return jobid

--- browserstack/controllers/browserstackController.js
+++ browserstack/controllers/browserstackController.js
@@ -24,51 +24,82 @@ exports.getScreenShot = function(req, res){
 
     screenshotClient.getBrowsers(function(error, browsers) {
         var latestChromeBrowser = GetWindows7LatestChromeBrowser(browsers);
+        var iosBrowser = {"os":"ios","os_version":"8.3","browser":"Mobile Safari","device":"iPhone 6 Plus","browser_version":null};
         var browsersToScreenshot = [];
         browsersToScreenshot.push(latestChromeBrowser);
+        browsersToScreenshot.push(iosBrowser);
 
         var options = {};
         options.browsers = browsersToScreenshot;
         options.url = url;
         options.local = true;
+        options.user_id = user_id;
 
         // use screenshot api
-        console.log(options);
-        screenshotClient.generateScreenshots(options, generateScreenshotsCallback);
-        res.json(user_id + 'screen for ' + url +' is processing');
+        //res.json(user_id + 'screen for ' + url +' is processing' + job.job_id);
+        screenshotClient.generateScreenshots(options, function(error, job){
+            if(error) {
+                console.log(error.stack);
+            }else{
+                res.json({
+                    userId: user_id,
+                    url: url,
+                    jobid: job.job_id
+                })
+                getScreenShot = setInterval(function() {
+                        screenshotClient.getJob(job.job_id, screenShotJobCallcallback)
+                        count ++;
+                    }
+                    , 3000);
+            }
+        });
+
+
 
     });
 
 };
 
-
+var SlackScreenShotEndPoint = 'http://10.228.150.158:1234/BrowserstackScreenshotComplete';
 function screenShotJobCallcallback(error, job) {
 
     if(error) {
         console.log(error.stack);
     }else{
-        debugger
-        if(job.screenshots[0].image_url) {
-            web.chat.postMessage('#general', job.screenshots[0].image_url, function (err, res) {
-                if (err) {
-                    console.log('Error:', err);
-                } else {
-                    console.log(count);
-                    console.log('Message sent: ', res);
 
-                }
-            });
+
+
+        if(job.screenshots[0].image_url) {
+            console.log(job);
+            // web.chat.postMessage('#general', job, function (err, res) {
+            //     if (err) {
+            //         console.log('Error:', err);
+            //     } else {
+            //         console.log(count);
+            //         console.log('Message sent: ', res);
+            //
+            //     }
+            // // });
+            // request.post(
+            //     SlackScreenShotEndPoint,
+            //     { json: { url: url, username: user } },
+            //     function (error, response, body) {
+            //         if (!error && response.statusCode == 200) {
+            //             console.log(body)
+            //         }
+            //     }
+            // );
             clearInterval(getScreenShot);
 
         }
-        console.log(job);
+       // console.log(job);
     }
 }
 
 var getScreenShot;
 var count = 0;
-function generateScreenshotsCallback(error, job) {
-    debugger
+function generateScreenshotsCallback(error, job ) {
+
     if(error) {
         console.log(error.stack);
     }else{
@@ -87,6 +118,31 @@ function GetWindows7LatestChromeBrowser(browsers){
     var chromeBrowsers = browsers.filter(function (el) {
         return el.browser == 'chrome',
         el.os_version == '7'}
+    );
+
+    var highestVersion = 0.0;
+    for(var i = 0; i < chromeBrowsers.length - 1; i++) {
+        var version = chromeBrowsers[i].browser_version;
+        if(!isNaN(version)) {
+            if(parseFloat(version) > highestVersion) {
+                highestVersion = parseFloat(version);
+            }
+        }
+    }
+
+    var latestBrowser = chromeBrowsers.filter(function (el) {
+        return el.browser_version == highestVersion}
+    )[0];
+
+    return(latestBrowser);
+}
+
+function GetiosBrowser(browsers){
+    // filter chrome browsers
+
+    var chromeBrowsers = browsers.filter(function (el) {
+        return el.browser == 'Mobile Safari',
+        el.os_version == '8.3'}
     );
 
     var highestVersion = 0.0;
