5aad11b46529528075d43fbed812f9d8d98ed17d
Ray Tam <rtam@kbb.com>
2017-05-28 23:50:12 -0700

    new branch, stub out functions

--- browserstack/server.js
+++ browserstack/server.js
@@ -1,15 +1,156 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 300,
+    port = 300,
     bodyParser = require('body-parser');
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./routes/browserstackRountes');
-routes(app);
+var BrowserStack = require("browserstack");
+var BrowserStackCredentials = require("./BrowserStackCredentials.js");
+var screenshotClient = BrowserStack.createScreenshotClient(BrowserStackCredentials.BrowserStackCredentials());
+var restClient = BrowserStack.createClient(BrowserStackCredentials.BrowserStackCredentials());
 
-app.listen(port);
+var websiteUrl = "https://www.google.com";
 
-console.log('RESTful API server started on: ' + port);
+function SubmitJobToBrowserStack(data) {
+    // data should contain the screenshot urls, the browser array, and the callback url
+    // submit these details to browserstack
+
+    /*var options = {};
+    options.browsers = browsersToScreenshot;
+    options.url = url;
+    options.local = true;
+    options.user_id = user_id;
+
+    // use screenshot api
+    //res.json(user_id + 'screen for ' + url +' is processing' + job.job_id);
+    console.log("send job to browserstack for url: " + options.url);
+    screenshotClient.generateScreenshots(options, function(error, job){
+        if(error) {
+
+        }else{
+            res.json({
+                userId: user_id,
+                url: url,
+                jobid: job.job_id
+            })
+            getScreenShot = setInterval(function() {
+                    screenshotClient.getJob(job.job_id, screenShotJobCallcallback)
+                }
+                , 3000);
+        }
+    });*/
+}
+
+function getAllPlatormBrowsers(){
+    var browsers = [];
+    browsers.push.apply(browsers, getDesktopBrowsers());
+    browsers.push.apply(browsers, getTabletBrowsers());
+    browsers.push.apply(browsers, getSmartphoneBrowsers());
+    return browsers;
+}
+
+function getTabletBrowsers(){
+    var browsers = [];
+    browsers.push(getLatest_Tablet_IOS_Browser());
+    browsers.push(getLatest_Tablet_Android_Browser());
+    return browsers;
+}
+
+function getLatest_Tablet_IOS_Browser() {
+
+}
+
+function getLatest_Tablet_Android_Browser() {
+
+}
+
+
+function getSmartphoneBrowsers(){
+    var browsers = [];
+    browsers.push(getLatest_Smartphone_IOS_Browser());
+    browsers.push(getLatest_Smartphone_Android_Browser());
+    return browsers;
+}
+
+function getLatest_Smartphone_IOS_Browser() {
+
+}
+
+function getLatest_Smartphone_Android_Browser() {
+
+}
+
+function getDesktopBrowsers(){
+    var browsers = [];
+    browsers.push(getLatest_Desktop_Firefox_Browser());
+    browsers.push(getLatest_Desktop_Chrome_Browser());
+    browsers.push(getLatest_Desktop_Edge_Browser());
+    browsers.push(getLatest_Desktop_IE_Browser());
+    return browsers;
+}
+
+function getLatest_Desktop_Firefox_Browser(){
+
+}
+
+function getLatest_Desktop_Chrome_Browser(){
+/*
+ // filter chrome browsers
+
+ var chromeBrowsers = browsers.filter(function (el) {
+ return el.browser == 'chrome',
+ el.os_version == '7'}
+ );
+
+ var highestVersion = 0.0;
+ for(var i = 0; i < chromeBrowsers.length - 1; i++) {
+ var version = chromeBrowsers[i].browser_version;
+ if(!isNaN(version)) {
+ if(parseFloat(version) > highestVersion) {
+ highestVersion = parseFloat(version);
+ }
+ }
+ }
+
+ var latestBrowser = chromeBrowsers.filter(function (el) {
+ return el.browser_version == highestVersion}
+ )[0];
+
+ return(latestBrowser);
+ */
+}
+
+function getLatest_Desktop_Edge_Browser(){
+
+}
+
+function getLatest_Desktop_IE_Browser(){
+
+}
+
+function getBrowsers(){
+    screenshotClient.getBrowsers(function(error, browsers) {
+
+    });
+}
+
+/// Routes ///
+// this is the route to receive the job from the server, the job receiver is get only
+app.route('/SubmitJob')
+    .get(bodyParser.urlencoded({ extended: true }), function (req, res) {
+        onsole.log('Browserstack SubmitJob');
+        console.log(req.body);
+        // receive url for file containing job details
+        // file should have job details (urls, browser, callback url)
+        // submit job to browserstack with given callback
+    })
+
+app.listen(PORT, function (err) {
+    if (err) {
+        return console.error('Error starting server: ', err)
+    }
+    console.log('Slack Server successfully started on port %s', PORT)
+})
+/// End Routes ///
--- slackapp/server.js
+++ slackapp/server.js
@@ -1,15 +1,96 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 1234,
+    port = 1234,
     bodyParser = require('body-parser');
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var controller = require('./slackController');
-controller(app);
 
-app.listen(port);
+var request = require('request');
+var slackToken = require("./SlackCredentials.js");
+var token = slackToken.SlackCredentials.token;
+var WebClient = require('@slack/client').WebClient;
+var web = new WebClient(token);
 
-console.log('Slack API server started on: ' + port);
+var BroserStack_SubmitJob = 'http://www.chefmoomoo.com:300/SubmitJob'
+var BrowserStack_JobComplete = 'http://www.chefmoomoo.com:1234/BrowserStack_JobComplete'
+
+// receive command from slack app, cmd is cmd parameters in json format
+function Slack_ReceiveMooMooCommand(cmd){
+    // we got a /moomoo command, save the urls and the command origin for reference
+    // and send the user a card asking for which platforms to test
+}
+
+// sends the message card to user asking them what browsers they want to generate screenshot in
+function Slack_SendMessageCard(user) {
+    // ask user what they want the screenshots in.
+    // options are: "Desktop, Smartphone, Tablet, All"
+}
+
+// this function receives the response from the slack message card
+function Slack_ReceiveMessageCard(platform) {
+    // submit the listed urls to browserstack with the specified platforms
+    // desktop, smartphone, tablet, or all
+    // use BrowserStack_JobComplete as a callback url
+
+}
+
+/// Routes ///
+// browserstack will call back to this function when it's done generating screenshots
+app.route('/Browserstack_JobComplete')
+    .get(function (req, res) {
+        res.sendStatus(200)
+    })
+    .post(bodyParser.urlencoded({ extended: true }), function (req, res) {
+        console.log('Browserstack Job Complete');
+        console.log(req.body);
+        // post the screenshot urls back to the channel
+    })
+
+// Slack moomoo command receiver
+app.route('/Slack_MooMoo')
+    .get(function (req, res) {
+        res.sendStatus(200)
+    })
+    .post(bodyParser.urlencoded({ extended: true }), function (req, res) {
+        console.log('MooMoo Command Received From Slack');
+        console.log(req.body);
+        /*
+         .post(bodyParser.urlencoded({ extended: true }), function (req, res) {
+         //if (req.body.token !== VERIFY_TOKEN) {
+         //return res.sendStatus(401)
+         //}
+
+         var url = req.body.text;
+         var user = req.body.user_name;
+         var message = 'Requested by ' + user + '.  Generating screenshots for: ' + url;
+
+         console.log(req.body);
+
+         res.json({
+         response_type: 'in_channel',
+         text: message
+         })
+
+         // post to browserStackGenerateScreenShotEndPoint
+         request.post(
+         browserStackGenerateScreenShotEndPoint,
+         { json: { url: url, username: user, timestamp: new Date().getTime() } },
+         function (error, response, body) {
+         if (!error && response.statusCode == 200) {
+         console.log(body);
+         }
+         }
+         );
+         })
+         */
+    })
+
+app.listen(PORT, function (err) {
+    if (err) {
+        return console.error('Error starting server: ', err)
+    }
+    console.log('Slack Server successfully started on port %s', PORT)
+})
+/// End Routes ///
