7fbf21baec17b5c846b21e8479cde2db68662c63
Ray Tam <rtam@kbb.com>
2017-06-04 18:30:02 -0700

    completed all devices for platforms

--- browserstack/server.js
+++ browserstack/server.js
@@ -79,7 +79,7 @@ function getBrowsers_callback(error, browsers) {
 
         screenshotClient.generateScreenshots(options, function (error, job) {
             if (error) {
-                
+
             } else {
 
             }
@@ -88,138 +88,15 @@ function getBrowsers_callback(error, browsers) {
         return true;
     }
 }
-
-function getAllPlatormBrowsers(){
-    var browsers = [];
-    browsers.push.apply(browsers, getDesktopBrowsers());
-    browsers.push.apply(browsers, getTabletBrowsers());
-    browsers.push.apply(browsers, getSmartphoneBrowsers());
-    return browsers;
-}
-
-function getTabletBrowsers(allBrowsers){
-    var iosBrowsers = allBrowsers.filter(function(browser){
-        return  browser.os === 'ios'
-    });
-    var androidBrowsers = allBrowsers.filter(function(browser){
-        return  browser.os === 'android'
-    });
-    var browsers = [];
-    browsers.push(getLatest_Tablet_IOS_Browser(iosBrowsers));
-    browsers.push(getLatest_Tablet_Android_Browser());
-    return browsers;
-}
-
-function getLatest_Tablet_IOS_Browser(iosBrowsers) {
-    var browserList = iosBrowsers.filter(function(browser){
-        return browser.device.includes('iPad');
-    });
-
-    var newList=  browserList.sort(function(a, b){
-        return a.device > b.device? 1: -1;
-    });
-    return newList[newList.length-1]
-}
-
-function getLatest_Tablet_Android_Browser() {
-
-}
-
-
-function getSmartphoneBrowsers(allBrowsers){
-    var iosBrowsers = allBrowsers.filter(function(browser){
-        return  browser.os === 'ios'
-    });
-    var androidBrowsers = allBrowsers.filter(function(browser){
-        return  browser.os === 'android'
-    });
-
-
-    var browsers = [];
-
-    browsers.push(getLatest_Smartphone_IOS_Browser(iosBrowsers));
-   // browsers.push(getLatest_Smartphone_Android_Browser());
-    return browsers;
-}
-
-function getLatest_Smartphone_IOS_Browser(iosBrowsers) {
-    var browserList = iosBrowsers.filter(function(browser){
-        return browser.device.includes('iPhone');
-    });
-
-    var newList = browserList.sort(function(a, b){
-        return a.device > b.device? 1: -1;
-    });
-    return newList[newList.length-1]
-}
-
-function getLatest_Smartphone_Android_Browser() {
-
-}
-
-function getLasteVersion(desktopBrowsers, browserName){
-    var browserList = desktopBrowsers.filter(function(browser){
-        return browser.browser === browserName && browser !== null;
-    });
-
-    return browserList.reduce(function(newversion, current){
-        return parseFloat(current.browser_version) > parseFloat(newversion.browser_version) ? current: newversion;
-    });
-}
-
-function getDesktopBrowsers(allBrowsers){
-    var desktopBrowsers = allBrowsers.filter(function(browser){
-       return  browser.os === 'Windows'
-    });
-    var browsers = [];
-    browsers.push(getLasteVersion(desktopBrowsers,'firefox'));
-    browsers.push(getLasteVersion(desktopBrowsers,'chrome'));
-    browsers.push(getLasteVersion(desktopBrowsers,'edge'));
-    browsers.push(getLasteVersion(desktopBrowsers,'ie'));
-    return browsers;
-}
-
-function getBrowsers(){
-    screenshotClient.getBrowsers(function(error, browsers) {
-
-    });
-}
-app.route('/browser')
+/// Routes ///
+// gets a JSON of available browsers
+app.route('/browsers')
     .get(bodyParser.urlencoded({ extended: true }), function (req, res) {
         screenshotClient.getBrowsers(function(error, browsers) {
             res.send(browsers);
         });
     })
-    .post(bodyParser.urlencoded({ extended: true }), function (req, res) {
 
-        screenshotClient.getBrowsers(function(error, browsers) {
-
-            switch(req.body.browserType) {
-                case 'desktop':
-                    var desktopBrowsers = getDesktopBrowsers(browsers);
-                    res.send(desktopBrowsers);
-                    break;
-                case 'tablet':
-                    var tabletBrowsers = getTabletBrowsers(browsers);
-                    res.send(tabletBrowsers);
-                    break;
-                case 'smartphone':
-                    var smartphoneBrowsers = getSmartphoneBrowsers(browsers);
-                    res.send(smartphoneBrowsers);
-                    break;
-                default:
-                   // code block
-            }
-
-
-
-        });
-
-
-        //res.send(browserType);
-    })
-
-/// Routes ///
 // this is the route to receive the job from the slack app, the job receiver is get only
 app.route('/submitjob')
     .get(bodyParser.urlencoded({ extended: true }), function (req, res) {
@@ -242,3 +119,107 @@ app.listen(port, function (err) {
     console.log('BrowserStack Server successfully started on port %s', port)
 })
 /// End Routes ///
+
+/* *** HANDLE BROWSER STUFF *** */
+
+function getAllPlatormBrowsers(allBrowsers){
+    var browsers = [];
+    browsers.push.apply(browsers, getDesktopBrowsers(allBrowsers));
+    browsers.push.apply(browsers, getTabletBrowsers(allBrowsers));
+    browsers.push.apply(browsers, getSmartphoneBrowsers(allBrowsers));
+    return browsers;
+}
+
+function getTabletBrowsers(allBrowsers){
+    var browsers = [];
+    browsers.push(getLatest_Tablet_IOS_Browser(allBrowsers));
+    browsers.push(getLatest_Tablet_Android_Browser(allBrowsers));
+    return browsers;
+}
+
+function getLatest_Tablet_IOS_Browser(allBrowsers) {
+    // filter for iPad but exclude iPad Minis and iPad Airs
+    var tabletIosBrowsers = allBrowsers.filter(function(browser){
+        return browser.device != null && browser.device.indexOf('iPad') > -1 && browser.device.indexOf('Mini') == -1  && browser.device.indexOf('Air') == -1
+    });
+
+    // sort by device, this will hopefully get us the latest ipad release at the end of the array
+    tabletIosBrowsers.sort(sortByDeviceAlphabetically);
+    return(tabletIosBrowsers[tabletIosBrowsers.length - 1]);
+}
+
+function getLatest_Tablet_Android_Browser(allBrowsers) {
+    // filter for Samsung Galaxy Tab
+    var tabletAndroidBrowsers = allBrowsers.filter(function(browser){
+        return browser.device != null && browser.device.indexOf('Samsung Galaxy Tab') > -1
+    });
+
+    // sort by device, this will hopefully get us the latest samsung galaxy tab release at the end of the array
+    tabletAndroidBrowsers.sort(sortByDeviceAlphabetically);
+    return(tabletAndroidBrowsers[tabletAndroidBrowsers.length - 1]);
+}
+
+function getSmartphoneBrowsers(allBrowsers){
+    var browsers = [];
+    browsers.push(getLatest_Smartphone_IOS_Browser(allBrowsers));
+    browsers.push(getLatest_Smartphone_Android_Browser(allBrowsers));
+    return browsers;
+}
+
+function getLatest_Smartphone_IOS_Browser(allBrowsers) {
+    // filter for iPhone but exclude iPhone Plus
+    var smartPhoneIosBrowsers = allBrowsers.filter(function(browser){
+        return browser.device != null && browser.device.indexOf('iPhone') > -1 && browser.device.indexOf('Plus') == -1
+    });
+
+    // sort by device, this will hopefully get us the latest iPhone release at the end of the array
+    smartPhoneIosBrowsers.sort(sortByDeviceAlphabetically);
+    return(smartPhoneIosBrowsers[smartPhoneIosBrowsers.length - 1]);
+}
+
+function getLatest_Smartphone_Android_Browser(allBrowsers) {
+    // filter for Samsung Galaxy but exclude Samsung Galaxy Tab and Mini
+    var smartPhoneAndroidBrowsers = allBrowsers.filter(function(browser){
+        return browser.device != null && browser.device.indexOf('Samsung Galaxy') > -1 && browser.device.indexOf('Tab') == -1  && browser.device.indexOf('Mini') == -1
+    });
+
+    // sort by device, this will hopefully get us the latest Samsung Galaxy release at the end of the array
+    smartPhoneAndroidBrowsers.sort(sortByDeviceAlphabetically);
+    return(smartPhoneAndroidBrowsers[smartPhoneAndroidBrowsers.length - 1]);
+}
+
+function getLastestVersion(browsers, browserName){
+    var browserList = browsers.filter(function(browser){
+        return browser.browser === browserName && browser !== null;
+    });
+
+    return browserList.reduce(function(newVersion, current){
+        return parseFloat(current.browser_version) > parseFloat(newVersion.browser_version) ? current: newVersion;
+    });
+}
+
+function getDesktopBrowsers(allBrowsers){
+    var desktopBrowsers = allBrowsers.filter(function(browser){
+        return browser.os === 'Windows'
+    });
+    var browsers = [];
+    browsers.push(getLastestVersion(desktopBrowsers,'firefox'));
+    browsers.push(getLastestVersion(desktopBrowsers,'chrome'));
+    browsers.push(getLastestVersion(desktopBrowsers,'edge'));
+    browsers.push(getLastestVersion(desktopBrowsers,'ie'));
+    return browsers;
+}
+
+/* *** HELPERS *** */
+
+function sortByDeviceAlphabetically(a,b) {
+    if (a.device < b.device)
+        return -1;
+    if (a.device > b.device)
+        return 1;
+    return 0;
+}
+
+/* *** END HELPERS *** */
+
+/* *** END BROWSER STUFF *** */
