3df39a8afafd022206ec5a578b4b315389595c2d
raymond.tam <raymond.tam@kbb.com>
2017-06-14 20:24:10 -0700

    fwefawefweaf

--- slackapp/server.js
+++ slackapp/server.js
@@ -54,7 +54,7 @@ function Slack_SendMessageCard(data) {
         }
     }
 
-    console.log(options.json.attachments);
+    console.log(options.json.attachments[0].actions);
 
     // send card
     request(options, function (error, response, body) {
